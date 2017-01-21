06a41a03c3e62069c7e3030e9c48e9159e871058
pawelaglowacki <pawelglowackicapgemini@gmail.com>
2017-01-21 18:05:46 +0300

    got first correct picture address

---
+++ webscraping.py
@@ -0,0 +1,12 @@
+import urllib2
+#from BeautifulSoup import BeautifulSoup
+# or if you're using BeautifulSoup4:
+from bs4 import BeautifulSoup
+
+page = urllib2.urlopen('http://pugs.tumblr.com/').read()
+
+soup = BeautifulSoup(page)
+soup.prettify()
+
+pugs = soup.findAll(attrs={'class' : 'photo'})
+print pugs[0]['src']
